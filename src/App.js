f414c9186a5f0b7b760bf92a357d9075774e3e22
GikariDigital <dihuthia@gmail.com>
2025-08-04 16:16:39 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Vehicle Import Tax Calculator</h1>
+      <p>This is a working version with dropdowns and logic to be added.</p>
+    </div>
+  );
+};
+
+export default App;
